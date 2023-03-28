ef73108ac860adbe84cc61b149462f534f412346
AnthonyScott <anthony.scott1005@gmail.com>
2023-03-28 11:56:46 -0400

    add index view and form for creating a recipe

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,9 +1,17 @@
 const Recipe = require("../models/Recipe");
 module.exports = {
   index,
+  new: createRecipe
 };
 
 function index(req, res, next) {
-  console.log("We're hitting the index page");
-  res.render("/");
+  res.render("recipes/index", {
+    title: "All Recipes"
+  });
+}
+
+function createRecipe(req, res, next){
+  res.render('recipes/new', {
+    title: "Add a Recipe"
+  })
 }
