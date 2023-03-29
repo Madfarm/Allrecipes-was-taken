fad0291e14c38e0bdaca87e7bbc5fc3f43398031
nohatekate <kateiscoding@gmail.com>
2023-03-29 09:02:38 -0500

    render page for each users recipes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const usersCtrl = require('../controllers/users')
+const ensureLoggedIn = require('../configs/ensureLoggedIn')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:userId/myrecipes', ensureLoggedIn, usersCtrl.index)
+
+
 
 module.exports = router;
