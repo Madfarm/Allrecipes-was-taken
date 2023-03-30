caea6ed32faee87c24e6893ed52723dbb7b3c758
nohatekate <kateiscoding@gmail.com>
2023-03-30 11:13:47 -0500

    require router in server and mount router

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ require('./configs/passport');
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const recipesRouter = require("./routes/recipes");
+const reviewsRouter = require('./routes/reviews')
 
 const app = express();
 
@@ -45,6 +46,7 @@ app.use(function(req, res, next){
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/recipes", recipesRouter);
+app.use("/", reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
